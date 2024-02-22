7f841cebd3139536b5f203b8bc924ae6dbfb5f18
pandekokko <119642530+pandekokko@users.noreply.github.com>
2024-02-22 07:28:27 +0000

    changed back to defaults

--- startup.sh
+++ startup.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-pip install -r requirements.txt
-streamlit run app.py --server.port 8000 --server.address 0.0.0.0
+python -m streamlit app.py --server.port 8000 --server.address 0.0.0.0
