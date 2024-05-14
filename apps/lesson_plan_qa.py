57d54d5538b0301486de28eff52e373d2c179feb
suzyleedto <119644155+suzyleedto@users.noreply.github.com>
2024-05-14 16:54:56 +0000

    test query doc

--- apps/lesson_plan_qa.py
+++ apps/lesson_plan_qa.py
@@ -43,6 +43,9 @@ from langchain_text_splitters import MarkdownHeaderTextSplitter
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.prompts import PromptTemplate
 from langchain.prompts import SystemMessagePromptTemplate, HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.callbacks.base import BaseCallbackHandler
+from langchain.memory import ConversationBufferMemory
+from langchain.memory.chat_message_histories import StreamlitChatMessageHistory
 
 
 
@@ -58,6 +61,40 @@ openai.organization = "org-ydtCQcRROzj3YuGKoh4NtXEV"
 openai_api_key = st.secrets["OPENAI_API_KEY"]
 
 
+class StreamHandler(BaseCallbackHandler):
+    def __init__(self, container: st.delta_generator.DeltaGenerator, initial_text: str = ""):
+        self.container = container
+        self.text = initial_text
+        self.run_id_ignore_token = None
+
+    def on_llm_start(self, serialized: dict, prompts: list, **kwargs):
+        # Workaround to prevent showing the rephrased question as output
+        if prompts[0].startswith("Human"):
+            self.run_id_ignore_token = kwargs.get("run_id")
+
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        if self.run_id_ignore_token == kwargs.get("run_id", False):
+            return
+        self.text += token
+        self.container.markdown(self.text)
+
+
+class PrintRetrievalHandler(BaseCallbackHandler):
+    def __init__(self, container):
+        self.status = container.status("**Context Retrieval**")
+
+    def on_retriever_start(self, serialized: dict, query: str, **kwargs):
+        self.status.write(f"**Question:** {query}")
+        self.status.update(label=f"**Context Retrieval:** {query}")
+
+    def on_retriever_end(self, documents, **kwargs):
+        for idx, doc in enumerate(documents):
+            source = os.path.basename(doc.metadata["source"])
+            self.status.write(f"**Document {idx} from {source}**")
+            self.status.markdown(doc.page_content)
+        self.status.update(state="complete")
+
+
 
 def qa_file(splits):
     general_system_template = """You are a Quality Assurance assistant, checking the quality of the lesson plan for a course.  
@@ -70,54 +107,38 @@ def qa_file(splits):
             HumanMessagePromptTemplate.from_template(general_user_template)
 ]
     qa_prompt = ChatPromptTemplate.from_messages( messages )
+
+    embeddings = OpenAIEmbeddings()
+    db = Chroma.from_documents(splits, embeddings)
+    retriever = db.as_retriever(search_type = "similarity", search_kwargs = {"k":10})
     
     if 'chain' not in st.session_state:
-        embeddings = OpenAIEmbeddings()
-        db = Chroma.from_documents(splits, embeddings)
-        retriever = db.as_retriever(search_type = "similarity", search_kwargs = {"k":15})
-        chain = ConversationalRetrievalChain.from_llm(llm = ChatOpenAI(temperature=0.5,model = 'gpt-4-turbo-2024-04-09', openai_api_key=openai_api_key),
-                                                                                 chain_type = "stuff",combine_docs_chain_kwargs={"prompt": qa_prompt},verbose=True,retriever=retriever)
+        msgs = StreamlitChatMessageHistory()
+        memory = ConversationBufferMemory(memory_key="chat_history", chat_memory=msgs, return_messages=True)
+        # Setup LLM and QA chain
+        llm = ChatOpenAI(
+        model_name="gpt-3.5-turbo", openai_api_key=openai_api_key, temperature=0, streaming=True
+        )
+        chain = ConversationalRetrievalChain.from_llm(
+            llm, retriever=retriever, memory=memory, verbose=True, chain = "stuff")
+        
+        if len(msgs.messages) == 0 or st.sidebar.button("Clear message history"):
+            msgs.clear()
+            msgs.add_ai_message("How can I help you?")
+
+        avatars = {"human": "user", "ai": "assistant"}
+        for msg in msgs.messages:
+            st.chat_message(avatars[msg.type]).write(msg.content)
+        if user_query := st.chat_input(placeholder="Ask me anything!"):
+            st.chat_message("user").write(user_query)
+
+            with st.chat_message("assistant"):
+                retrieval_handler = PrintRetrievalHandler(st.container())
+                stream_handler = StreamHandler(st.empty())
+                response = chain.run(user_query, callbacks=[retrieval_handler, stream_handler])
     
         st.session_state['chain'] = chain 
-    def conversational_chat(query):
-        print("sending q")  
-        chain =  st.session_state['chain']
-        result = chain({"question": query, "chat_history": st.session_state['history']})
-        st.session_state['history'].append((query, result["answer"]))
-                
-        return result["answer"]
-            
-    if 'history' not in st.session_state:
-        st.session_state['history'] = []
-
-    if 'generated' not in st.session_state:
-        st.session_state['generated'] = ["Hello ! Ask me anything about the lesson plan 🤗"]
-
-    if 'past' not in st.session_state:
-        st.session_state['past'] = ["Hey ! 👋"]
-                
-        #container for the chat history
-    response_container = st.container()
-        #container for the user's text input
-    container = st.container()
-
-    with container:
-        with st.form(key='my_form', clear_on_submit=True):
-            user_input = st.text_input("Query:", placeholder="Ask your question here (:", key='input')
-            submit_button = st.form_submit_button(label='Send', on_click=None)
-                
-            if submit_button and user_input:
-                output = conversational_chat(user_input)
-                    
-                st.session_state['past'].append(user_input)
-                st.session_state['generated'].append(output)
-
-        if st.session_state['generated']:
-            with response_container:
-                for i in range(len(st.session_state['generated'])):
-                    message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="big-smile")
-                    message(st.session_state["generated"][i], key=str(i),avatar_style="initials", seed = "EDNA")
-
+    
 
 def split_text_semantic(text):
     text_splitter = SemanticChunker(OpenAIEmbeddings())
