57baccbdea3be4feb05fb153a51dbad93d11f31c
suzyleedto <119644155+suzyleedto@users.noreply.github.com>
2024-05-16 06:38:50 +0000

    input text height

--- apps/lesson_plan_qa.py
+++ apps/lesson_plan_qa.py
@@ -152,7 +152,7 @@ def qa_file(splits):
         #container for the chat history
     response_container = st.container()
         #container for the user's text input
-    container = st.container()
+    container = st.container(height=1000)
     with response_container:
         for msg in msgs.messages:
             st.chat_message(avatars[msg.type]).write(msg.content)
