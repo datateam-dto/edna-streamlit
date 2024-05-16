16795ac0b6146bf1f882155a22d4059a7964c3d4
suzyleedto <119644155+suzyleedto@users.noreply.github.com>
2024-05-16 12:13:40 +0000

    debug

--- apps/lesson_guide_qa.py
+++ apps/lesson_guide_qa.py
@@ -236,8 +236,8 @@ def main():
         uploaded_file = st.file_uploader("Choose a file (pdf)", type=["pdf"], help="file to be parsed")
     if uploaded_file is not None :
         content = extract_text_(uploaded_file)
-        st.markdown(content)
         md_text = convert_to_markdown(content)  
+        st.markdown(content)
         splits = split_text_markdown(md_text)
         #st.write(splits)
         #qa_file(splits)
