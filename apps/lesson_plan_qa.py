5a4159ee907ca68e1b3fd98a80925bbd0d7c6331
suzyleedto <119644155+suzyleedto@users.noreply.github.com>
2024-05-16 06:25:02 +0000

    debug dict

--- apps/lesson_plan_qa.py
+++ apps/lesson_plan_qa.py
@@ -161,6 +161,8 @@ def qa_file(splits):
         init_prompt = st.selectbox(
         'You might want to try these prompts, click to expand',
         prompts.get("lesson_plan").keys())
+        st.write(init_prompt)
+        st.write(prompts.get(init_prompt))
 
         instr = 'Hi there! "Ask your question here.'
         with st.form(key='my_form', clear_on_submit=True):
