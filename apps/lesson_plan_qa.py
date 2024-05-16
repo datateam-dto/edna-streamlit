16ed4347acf3bf7f059be2055de993cbb2a0ccad
suzyleedto <119644155+suzyleedto@users.noreply.github.com>
2024-05-16 03:50:17 +0000

    updated order of question

--- apps/lesson_plan_qa.py
+++ apps/lesson_plan_qa.py
@@ -156,8 +156,8 @@ def qa_file(splits):
             user_query = st.text_input("Query:", placeholder="Ask your question here (:", key='input')
             submit_button = st.form_submit_button(label='Send', on_click=None)
             if submit_button and user_query:
-                st.chat_message("user").write(user_query)
                 with response_container:
+                    st.chat_message("user").write(user_query)
                     with st.chat_message("assistant"):
                         retrieval_handler = PrintRetrievalHandler(st.container())
                         stream_handler = StreamHandler(st.empty())
